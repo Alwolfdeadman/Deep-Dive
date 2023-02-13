43c8fe4ed107ba5dcaab08ba13152bdaa1286331
Alwolfdeadman <alwolf.deadman@gmail.com>
2023-02-13 15:39:19 +0200

    First entry with working map and a player character that has an attack

---
+++ Game.py
@@ -0,0 +1,176 @@
+import arcade
+import Player
+from random import randint
+import time
+
+SCREEN_W = 1480
+SCREEN_H = 840
+
+""""
+-dobavi komentari
+- character sprite za dvigenie v vsichki posoki
+- ataka na geroq
+- enemi class
+"""
+
+
+class Game(arcade.Window):
+
+    def __init__(self, w, h, t):
+        super().__init__(w, h, t)  # call the parent class init to create the window
+
+        self.p_list = None
+        self.player = None
+        self.p_sprite = "assets/TMP/0x72_16x16DungeonTileset.v5/items/npc_dwarf.png"
+
+        self.room_number = 0
+        self.tile_map = None
+        self.room_name = None
+        self.num_enemies = 0
+        self.rooms_passed = 0
+        self.dungeon_size = 1
+
+        self.scene = None
+        self.physics_engine = None
+        
+        arcade.set_background_color(arcade.color.DARK_RED)
+        self.start_time = 0
+        self.last_button_press = 0
+
+    def setup(self):
+        r_stats = self.find_cords(self.room_number)
+        self.p_list = arcade.SpriteList()
+        self.player = Player.Player(self.p_sprite, r_stats[0], r_stats[1], SCREEN_W, SCREEN_H)
+        self.p_list.append(self.player)
+
+        self.room_name = f"assets/maps/room{self.room_number}.tmx"
+        layer_options = {
+            "walls": {
+                "use_spatial_hash": True,
+            }
+        }
+        self.tile_map = arcade.load_tilemap(self.room_name, 1, layer_options)
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
+        if self.tile_map.background_color:
+            arcade.set_background_color(self.tile_map.background_color)
+        if self.room_number == 2:
+            self.physics_engine = arcade.PhysicsEngineSimple(self.player, (self.scene.get_sprite_list("walls"),
+                                                                           self.scene.get_sprite_list("pit1")))
+        elif self.room_number == 6:
+            self.physics_engine = arcade.PhysicsEngineSimple(self.player, (self.scene.get_sprite_list("walls"),
+                                                                           self.scene.get_sprite_list("floor2")))
+        else:
+            self.physics_engine = arcade.PhysicsEngineSimple(self.player, self.scene.get_sprite_list("walls"))
+
+    def on_draw(self):
+        arcade.start_render()
+        self.scene.draw()
+        self.player.draw()
+        self.player.weapon_list.draw()
+
+        current_time = time.time()
+        if current_time - self.start_time >= 0.7:
+            for sword in self.player.weapon_list:
+                self.player.weapon_list.remove(sword)
+                self.start_time = current_time
+
+    def update(self, delta_time):
+        r_stats = self.find_cords(self.room_number)
+        self.player.update()
+        self.physics_engine.update()
+
+        if (
+                r_stats[2] <= self.player.center_x <= (r_stats[2] + 16)
+                and r_stats[3] <= self.player.center_y <= r_stats[3] + 16
+                and self.num_enemies == 0 and self.rooms_passed < self.dungeon_size - 1
+                or (
+                    r_stats[2] <= self.player.center_x <= (r_stats[2] + 48)
+                    and r_stats[3] <= self.player.center_y <= r_stats[3] + 16
+                    and self.num_enemies == 0 and self.rooms_passed < self.dungeon_size - 1
+                    and self.room_number == 2
+                   )
+           ):
+            self.room_number = randint(1, 10)
+            self.rooms_passed += 1
+            self.setup()
+        elif (
+                    r_stats[2] <= self.player.center_x <= (r_stats[2] + 16)
+                and r_stats[3] <= self.player.center_y <= r_stats[3] + 16
+                and self.num_enemies == 0 and self.rooms_passed == self.dungeon_size - 1
+             ):
+            self.room_number = 11
+            self.rooms_passed += 1
+            self.setup()
+        elif (
+                    r_stats[2] <= self.player.center_x <= (r_stats[2] + 16)
+                and r_stats[3]-16 <= self.player.center_y <= r_stats[3] + 16
+                and self.num_enemies == 0 and self.room_number == 11
+             ):
+            self.room_number = 12
+            self.rooms_passed += 1
+            self.setup()
+        elif (
+                    r_stats[2] <= self.player.center_x <= (r_stats[2] + 16)
+                and r_stats[3] <= self.player.center_y <= r_stats[3] + 16
+                and self.num_enemies == 0 and self.room_number == 12
+             ):
+            self.room_number = 0
+            self.rooms_passed = 0
+            self.setup()
+
+    def on_key_press(self, key, modifiers):
+        enemy = None
+        self.player.on_key_press(key, modifiers)
+        # if key == arcade.key.J:
+        #     self.player.attack()
+
+        current_time = time.time()
+        if current_time - self.last_button_press >= 0.5:
+            if key == arcade.key.J:
+                self.last_button_press = current_time
+                self.player.attack(enemy)
+
+    def on_key_release(self, key, modifiers):
+        self.player.on_key_release(key, modifiers)
+
+    def find_cords(self, room_numb):
+        tmp = []
+        match room_numb:
+            case 0:
+                tmp.extend([584, 264, 664, 442])
+            case 1:
+                tmp.extend([568, 76, 567, 614])
+            case 2:
+                tmp.extend([536, 474, 520, 494])
+            case 3:
+                tmp.extend([344, 234, 712, 440])
+            case 4:
+                tmp.extend([568, 442, 360, 264])
+            case 5:
+                tmp.extend([376, 344, 744, 360])
+            case 6:
+                tmp.extend([56, 316, 1000, 296])
+            case 7:
+                tmp.extend([568, 58, 200, 616])
+            case 8:
+                tmp.extend([568, 154, 554, 584])
+            case 9:
+                tmp.extend([184, 42, 184, 568])
+            case 10:
+                tmp.extend([232, 474, 426, 184])
+            case 11:
+                tmp.extend([184, 346, 872, 344])
+            case 12:
+                tmp.extend([568, 394, 552, 346])
+        return tmp
+
+
+def main():
+    window = Game(SCREEN_W, SCREEN_H, "asa")
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Player.py
@@ -0,0 +1,95 @@
+import arcade
+
+# fix the weapon so it isnt a sprite but item class and the damage calculation
+
+
+class Player(arcade.Sprite):
+    def __init__(self, path,  x, y, width, height):
+        super().__init__(path)
+        self.screen_w = width
+        self.screen_h = height
+        self.center_x = x
+        self.center_y = y
+
+        self.speed = 8
+        self.movement_distance = 0
+        self.movement_direction = None
+        self.look_dir = None
+        self.p_class = "k"
+
+        self.attack_power = 20
+        self.weapon = None
+        self.weapon_list = arcade.SpriteList()
+
+    def update(self):
+        if self.movement_distance > 0:
+            if self.movement_direction == "up":
+                self.center_y += self.speed
+            elif self.movement_direction == "down":
+                self.center_y -= self.speed
+            elif self.movement_direction == "left":
+                self.center_x -= self.speed
+            elif self.movement_direction == "right":
+                self.center_x += self.speed
+            self.movement_distance -= self.speed
+        else:
+            self.movement_direction = None
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > self.screen_w - 1:
+            self.right = self.screen_w - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > self.screen_h - 1:
+            self.top = self.screen_h - 1
+
+    def on_key_press(self, key, modifiers):
+        dist = 16
+        if key == arcade.key.W:
+            self.movement_direction = "up"
+            self.movement_distance = dist
+            self.look_dir = "up"
+        elif key == arcade.key.S:
+            self.movement_direction = "down"
+            self.movement_distance = dist
+            self.look_dir = "down"
+        elif key == arcade.key.A:
+            self.movement_direction = "left"
+            self.movement_distance = dist
+            self.look_dir = "left"
+        elif key == arcade.key.D:
+            self.movement_direction = "right"
+            self.movement_distance = dist
+            self.look_dir = "right"
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.W or key == arcade.key.S:
+            self.change_y = 0
+        elif key == arcade.key.A or key == arcade.key.D:
+            self.change_x = 0
+
+    def attack(self, enemy):
+        if self.look_dir == "up":
+            self.weapon = arcade.Sprite("assets/character_classes_and_animations/weapons/swords/sword_up.png")
+            self.weapon.center_x = self.center_x
+            self.weapon.center_y = self.center_y + 16
+        elif self.look_dir == "down":
+            self.weapon = arcade.Sprite("assets/character_classes_and_animations/weapons/swords/sword_down.png")
+            self.weapon.center_x = self.center_x
+            self.weapon.center_y = self.center_y - 17
+        elif self.look_dir == "left":
+            self.weapon = arcade.Sprite("assets/character_classes_and_animations/weapons/swords/sword_left.png")
+            self.weapon.center_x = self.center_x - 18
+            self.weapon.center_y = self.center_y
+        elif self.look_dir == "right":
+            self.weapon = arcade.Sprite("assets/character_classes_and_animations/weapons/swords/sword_right.png")
+            self.weapon.center_x = self.center_x + 16
+            self.weapon.center_y = self.center_y
+        else:
+            self.weapon = arcade.Sprite("assets/character_classes_and_animations/weapons/swords/sword_right.png")
+            self.weapon.center_x = self.center_x + 16
+            self.weapon.center_y = self.center_y
+        self.weapon_list.append(self.weapon)
+        # enemy.take_damage(self.attack_power)
