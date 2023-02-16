c94e7634c12dbd382f0153fb5e95d581617cc224
Alwolfdeadman <alwolf.deadman@gmail.com>
2023-02-16 02:14:16 +0200

    created the enemy class and started drawing them

--- Enemy.py
+++ Enemy.py
@@ -1,41 +1,23 @@
 import arcade
+from random import randint
 
 
 class Enemy(arcade.Sprite):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, num, room_count, x, y):
+        super().__init__(f"assets/melee_monsters/monsters_1/monster{num}.png")
+        self.center_x = x
+        self.center_y = y
+        self.health = randint(7, 10)*room_count
 
-        self.facing_direction = RIGHT_FACING
+        self.num_of_monster = num
 
-        # Used for image sequences
-        self.cur_texture = 0
-        self.scale = CHARACTER_SCALING
-
-        main_path = f":resources:images/animated_characters/{name_folder}/{name_file}"
-
-        self.idle_texture_pair = load_texture_pair(f"{main_path}_idle.png")
-        self.jump_texture_pair = load_texture_pair(f"{main_path}_jump.png")
-        self.fall_texture_pair = load_texture_pair(f"{main_path}_fall.png")
-
-        # Load textures for walking
-        self.walk_textures = []
-        for i in range(8):
-            texture = load_texture_pair(f"{main_path}_walk{i}.png")
-            self.walk_textures.append(texture)
-
-        # Load textures for climbing
-        self.climbing_textures = []
-        texture = arcade.load_texture(f"{main_path}_climb0.png")
-        self.climbing_textures.append(texture)
-        texture = arcade.load_texture(f"{main_path}_climb1.png")
-        self.climbing_textures.append(texture)
-
-        # Set the initial texture
-        self.texture = self.idle_texture_pair[0]
-
-        # Hit box will be set based on the first image used. If you want to specify
-        # a different hit box, you can do it like the code below.
-        # self.set_hit_box([[-22, -64], [22, -64], [22, 28], [-22, 28]])
-        self.set_hit_box(self.texture.hit_box_points)
-
+        self.damaging_textures = []
+        self.damaging_textures.append(
+            arcade.load_texture("assets/character_classes_and_animations/weapons/swords/sword_up.png"))
+        self.damaging_textures.append(
+            arcade.load_texture("assets/character_classes_and_animations/weapons/swords/sword_down.png"))
+        self.damaging_textures.append(
+            arcade.load_texture("assets/character_classes_and_animations/weapons/swords/sword_left.png"))
+        self.damaging_textures.append(
+            arcade.load_texture("assets/character_classes_and_animations/weapons/swords/sword_right.png"))
